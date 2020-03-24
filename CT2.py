4d12d4514e161c9229325cd200280c3675011ab4
Grace Keane <gracekeane82@gmail.com>
2020-03-24 14:51:26 +0000

    Needs more work - bugs in code

--- CT2.py
+++ CT2.py
@@ -1,6 +1,10 @@
 # Grace Keane
-# Cleaning and testing
+# Cleaning and testing Thompsons algorithm
 # Running the result of the script CT.py 
+
+# This python script can be used as a module so it can imported
+#on other python programs
+#It matches infix regular expressions to strings
 
 # Referance the script (CT.py)
 import CT
